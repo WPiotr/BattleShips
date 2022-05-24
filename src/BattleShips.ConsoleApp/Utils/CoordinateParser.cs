a80fcc83cd9ab5eb09c029455961e8cf2af1b937
Piotr Wyszkowski <6958884+WPiotr@users.noreply.github.com>
2022-05-24 09:33:16 +0200

    Add CoordinateParser class Add Unit tests for CoordinateParser

---
+++ src/BattleShips.ConsoleApp/Utils/CoordinateParser.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace BattleShips.ConsoleApp.Utils
+{
+    public static class CoordinateParser
+    {
+        public static bool TryParse(string input, out int? column, out int? row)
+        {
+            column = null;
+            row = null;
+            var trimmedInput = input.Trim();
+            var columnLetter = trimmedInput[0];
+            if (!char.IsLetter(columnLetter))
+            {
+                return false;
+            }
+
+            var rowPart = trimmedInput[1..];
+            if (rowPart.Count() > 2)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(rowPart, out var parsedRowNumber))
+            {
+                return false;
+            }
+
+            column = columnLetter - 64;
+            row = parsedRowNumber;
+            return true;
+        }
+    }
+}
---
+++ tests/BattleShips.UnitTests/ConsoleApp/CoordinateParserTests.cs
@@ -0,0 +1,43 @@
+﻿using BattleShips.ConsoleApp.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace BattleShips.UnitTests.ConsoleApp
+{
+    public class CoordinateParserTests
+    {
+        [Theory]
+        [InlineData("A1", 1, 1)]
+        [InlineData("B1", 2, 1)]
+        [InlineData("C1", 3, 1)]
+        [InlineData("D1", 4, 1)]
+        [InlineData("A5", 1, 5)]
+        [InlineData("A6", 1, 6)]
+        [InlineData(" A6", 1, 6)]
+        [InlineData(" A6 ", 1, 6)]
+        public void TryParse_CorrectValue_ReturnsTrue_And_Coordinate(string input, int expectedColumn, int expectedRow)
+        {
+            //Act
+            var result = CoordinateParser.TryParse(input, out var column, out var row);
+
+            //Assert
+            result.Should().BeTrue();
+            column.Should().Be(expectedColumn);
+            row.Should().Be(expectedRow);
+        }
+        
+        [Theory]
+        [InlineData("1")]
+        [InlineData("41")]
+        [InlineData(";1")]
+        [InlineData(";)12")]
+        public void TryParse_CorrectValue_ReturnsFalse(string input)
+        {
+            //Act
+            var result = CoordinateParser.TryParse(input, out var column, out var row);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+    }
+}
